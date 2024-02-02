f1228c421933dfd6ee5220f15133754cd45f6e5d
Samuel <samuelgomes101112@gmail.com>
2024-02-02 11:16:59 -0300

    Adicinando arquivo tabela-produtor.sql

---
+++ tabela-produtos.sql
@@ -0,0 +1,19 @@
+/*
+-- Query: SELECT * FROM serenatto.produtos
+LIMIT 0, 1000
+
+-- Date: 2024-02-02 11:15
+*/
+INSERT INTO `` (`id`,`tipo`,`nome`,`descricao`,`imagem`,`preco`) VALUES (1,'Café','Café Cremoso','Café cremoso irresistivelmente suave e que envolve seu paladar','cafe-cremoso.jpg',5.00);
+INSERT INTO `` (`id`,`tipo`,`nome`,`descricao`,`imagem`,`preco`) VALUES (2,'Café','Café com Leite','A harmonia perfeita do café e do leite, uma experiência reconfortante','cafe-com-leite.jpg',2.00);
+INSERT INTO `` (`id`,`tipo`,`nome`,`descricao`,`imagem`,`preco`) VALUES (3,'Café','Cappuccino','Café suave, leite cremoso e uma pitada de sabor adocicado','cappuccino.jpg',7.00);
+INSERT INTO `` (`id`,`tipo`,`nome`,`descricao`,`imagem`,`preco`) VALUES (4,'Café','Café Gelado','Café gelado refrescante, adoçado e com notas sutis de baunilha ou caramelo.','cafe-gelado.jpg',3.00);
+INSERT INTO `` (`id`,`tipo`,`nome`,`descricao`,`imagem`,`preco`) VALUES (6,'Almoço','Filé de peixe','Filé de peixe salmão assado, arroz, feijão verde e tomate.','prato-peixe.jpg',24.00);
+INSERT INTO `` (`id`,`tipo`,`nome`,`descricao`,`imagem`,`preco`) VALUES (7,'Almoço','Frango','Saboroso frango à milanesa com batatas fritas, salada de repolho e molho picante','prato-frango.jpg',23.00);
+INSERT INTO `` (`id`,`tipo`,`nome`,`descricao`,`imagem`,`preco`) VALUES (8,'Almoço','Fettuccine','Prato italiano autêntico da massa do fettuccine com peito de frango grelhado','fettuccine.jpg',22.50);
+INSERT INTO `` (`id`,`tipo`,`nome`,`descricao`,`imagem`,`preco`) VALUES (9,'Almoço','Macarronada','Com molho branco','logo-serenatto.png',25.00);
+INSERT INTO `` (`id`,`tipo`,`nome`,`descricao`,`imagem`,`preco`) VALUES (10,'Almoço','Lasanha','Lasanha com molho branco e vermelho','logo-serenatto.png',25.00);
+INSERT INTO `` (`id`,`tipo`,`nome`,`descricao`,`imagem`,`preco`) VALUES (18,'Café','sem ','sem','logo-serenatto.png',0.00);
+INSERT INTO `` (`id`,`tipo`,`nome`,`descricao`,`imagem`,`preco`) VALUES (20,'Café','com','ss','FOTO PERFIL.png65bcd30d44658',0.00);
+INSERT INTO `` (`id`,`tipo`,`nome`,`descricao`,`imagem`,`preco`) VALUES (21,'Café','com','ss','FOTO PERFIL.png65bcd328db2e4',0.00);
+INSERT INTO `` (`id`,`tipo`,`nome`,`descricao`,`imagem`,`preco`) VALUES (22,'Café','sss','sss','camisa modelo 2.png65bcd356a790d',0.00);
